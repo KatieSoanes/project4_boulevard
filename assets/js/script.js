49288d9183bb0c77ed018a8f3e675ff8a4055f96
KatieSoanes <katiesoanes@hotmail.com>
2022-04-28 11:15:06 +0000

    add comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,17 @@ function reserve()
     window.location= "reservation.html"
     setTimeout('myFunction()', 2000);
 }
+
+// // Scroll to top of page arrow
+
+$(window).scroll(function(){
+    if ($(this).btnScrollToTop() > 100) {
+        $('.btnScrollToTop').fadeIn();
+    } else {
+        $('.btnScrollToTop').fadeOut();
+    }
+});
+$('.btnScrollToTop').click(function(){
+    $('html, body').animate({scrollTop : 0},800);
+    return false;
+});
