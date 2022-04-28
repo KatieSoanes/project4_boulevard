6b03968e329fe7e5a1f5edda7c2481471c5a0721
KatieSoanes <katiesoanes@hotmail.com>
2022-04-28 11:23:27 +0000

    add javascript to scroll to top arrow

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,16 +6,23 @@ function reserve()
     setTimeout('myFunction()', 2000);
 }
 
-// // Scroll to top of page arrow
+// Scroll to top of page arrow
+// code based on W3Schools
+mybutton = document.getElementById("btnScrollToTop");
 
-$(window).scroll(function(){
-    if ($(this).btnScrollToTop() > 100) {
-        $('.btnScrollToTop').fadeIn();
-    } else {
-        $('.btnScrollToTop').fadeOut();
-    }
-});
-$('.btnScrollToTop').click(function(){
-    $('html, body').animate({scrollTop : 0},800);
-    return false;
-});
+// When the user scrolls down 20px from the top of the document, show the button
+// window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
