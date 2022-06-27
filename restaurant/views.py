962bcd61b4de3c92a81a815189c4df9a6ef031ec
KatieSoanes <katiesoanes@hotmail.com>
2022-06-27 21:27:33 +0000

    double booking

--- restaurant/views.py
+++ restaurant/views.py
@@ -29,6 +29,17 @@ def add_reservation(request):
     name = request.POST['name']
     email = request.POST['email']
     restaurant = Restaurant.objects.first()
+    no_of_tables = Restaurant.no_of_tables
+    reservations = Reservations.objects.all()
+
+    current_year = timezone.now().day
+    current_day = timezone.now().day
+    # double booking
+    if Reservation.objects.filter(time__day=current_day, time__hour=current_hour).count() > no_of_tables:
+        return render(request, 'restaurant/index.html', {
+            'error_message': "You didn't select a choice.",
+        })
+
     reservation = Reservation.objects.create(name=name, email=email,time=datetime.now(), restaurant=restaurant)
     
     return HttpResponseRedirect(reverse('restaurant:index'))
